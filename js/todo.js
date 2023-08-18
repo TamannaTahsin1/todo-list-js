0e06652fd695b3a60059a0e9ad9190ff820a19ab
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-08-18 22:51:02 +0600

    first commit

---
+++ js/todo.js
@@ -0,0 +1,39 @@
+// add text function
+function addText(){
+    const inputType =getInputValue('input-type');
+    const outputResult =inputType;
+    // get the value as output
+    setElementInnerText('output-result', outputResult)
+    // add the output to the list
+    addToList('Text', outputResult)
+}
+// reuseable input value function (for all)
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const inputValueText = inputField.value;
+
+    // to clear value
+    inputField.value= " ";
+
+    const value = inputValueText;
+    return value; 
+}
+// reuseable set span, p, div etc text (for all)
+    function setElementInnerText(elementId, area){
+    const element = document.getElementById(elementId);
+    element.value = area;
+    }
+//  add to the list
+function addToList(outputResult, action){
+    console.log(outputResult+ " " +action);
+    const outputValue = document.getElementById('output-result');
+    // creating element
+    const count = outputValue.childElementCount;
+    const li = document.createElement('li');
+    // li.classList.add('my-4');
+    // li.innerText = `${count +1}. ${outputResult} ${action}`;
+    li.classList.add('my-4');
+    li.innerHTML = `${count + 1}. ${outputResult} ${action} <button class="btn btn-sm btn-success">Convert</button>`;
+
+    outputValue.appendChild(li);
+}
